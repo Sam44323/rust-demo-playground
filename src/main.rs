4c7b1e2333408eedb719a32ac23808bfc2eea956
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-27 01:32:08 +0530

    add: added sizes description

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,7 @@ fn main() {
 
     // Example of a mutable variable
     let mut y = 10;
+
+    // Example of type description addition (i8, u8, i16, u16, i32, u32, i64, u64, f32, f64, isize, usize)
+    let x: u32 = 34;
 }
