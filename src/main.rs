5aa3ff2af61c165a974fa76cb96726f15de2472d
Suranjan <suranjanbiswas2002@gmail.com>
2021-08-28 13:14:26 +0530

    fix: fixed unused warnings error

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 fn main() {
+    /*
+    using _ for ignoring the unused variables warning
+    */
     // Everything is immutable by default
-    let x = 5;
+    let _x = 5;
 
     // Example of a mutable variable
-    let mut y = 10;
+    let mut _y = 10;
 
     // Example of type description addition (i8, u8, i16, u16, i32, u32, i64, u64, f32, f64, isize, usize)
-    let x: u32 = 34;
+    let _u: u32 = 34;
 
     // tuple declaration with certain types
     let t: (i32, f64, char) = (41, 6.14, 'j');
     // destructuring the tuple
-    let (_, _, x) = t; // _ means, I don't want that value (tuple can also be called by index such as t.0 or etc)
+    let (_, _, _x) = t; // _ means, I don't want that value (tuple can also be called by index such as t.0 or etc)
+
+    // example of nested tuple
+    let f_tuple = (1, 'a', false);
+    let s_tuple = (4, f_tuple);
+    println!("{}", s_tuple.0);
 }
