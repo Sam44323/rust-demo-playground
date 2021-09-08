6eb1e8c63222cb497ae8fea438955ea987462c22
Suranjan <suranjanbiswas2002@gmail.com>
2021-09-08 22:39:24 +0530

    add: adde area implementation for shape of enums

--- src/main.rs
+++ src/main.rs
@@ -204,6 +204,16 @@ enum Shape {
     Circle(u32),
 }
 
+impl Shape {
+    fn area(&self) -> f64 {
+        match *self {
+            Shape::Rectangle { width, height } => (width * height) as f64,
+            Shape::Square(ref s) => (s * s) as f64,
+            Shape::Circle(ref r) => 3.14 * (r * r) as f64,
+        }
+    }
+}
+
 fn main() {
     /*
     using _ for ignoring the unused variables warning
